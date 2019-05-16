e9f70b9e49e3a7509e75d371aa20c1a36e81dce3
Dodi Hidayat <dodih127@gmail.com>
2019-05-16 22:02:41 +0700

    first commit

---
+++ application/controllers/admin/Tabel.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Tabel extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('admin/konten/tabel');
+	}
+
+}
+
+/* End of file Tabel.php */
+/* Location: ./application/controllers/admin/Tabel.php */
---
+++ application/controllers/admin/Test.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Test extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('admin/home');
+	}
+   
+}
+
+/* End of file Test.php */
+/* Location: ./application/controllers/admin/Test.php */
